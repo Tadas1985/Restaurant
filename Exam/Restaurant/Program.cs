a53f27745ed81b3223614422a47da474bbbcda58
Tadas <tadas1985@gmail.com>
2022-03-04 13:07:34 +0200

    first commit

---
+++ Exam/Restaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restaurant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a restaurant management system");
+        }
+    }
+}
